53a4029453b1ee6392c06cd00e38125d24a0f335
Mark Chang <chang.m.mark@gmail.com>
2019-05-01 16:55:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var name = "Joe"
 var height = "74"
-if
+if (name === "Susan") {consol.log("The name is correct")} else {console.log("Expected the name to be Susan")}
 
 // Don't worry about this
 module.exports = { name, height
